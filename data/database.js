1c010070366b1f672c55a6a0be88427367e15201
unknown <mamraj09112002@gmail.com>
2023-07-21 22:28:47 +0530

    deploy issue

--- data/database.js
+++ data/database.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
-
-export const connectDB =()=>{
-    mongoose.connect(process.env.MONGO_URI, {
-  dbname: "backendapir",
+export const connectDB=()=> {
+    mongoose.connect(process.env.MONGO_URI,{
+    dbName:"backendapi",
 
 })
-  .then(() => console.log("Database connected"))
-  .catch((e) => console.log(e));
+.then((c)=> console.log(`Database Connected with ${c.connection.host}`))
+.catch((e)=> console.log(e));
+
 };
+
+
 
