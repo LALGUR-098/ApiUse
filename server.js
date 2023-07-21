b0ba89e57a3ddd8067a6f7329fd81a5a71a5a7b0
unknown <mamraj09112002@gmail.com>
2023-07-21 11:46:53 +0530

    MongoURI

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ import { connectDB } from "./data/database.js";
 connectDB();
 
 app.listen(5000,(req, res)=>{
-    console.log("Server is working");   
+    console.log(`server is working on port :${process.env.PORT} in ${process.env.NODE_ENV} mode`); 
 });
